183ac0b15ee2f4bcb3476e2f059888e6881f5bc9
zendau <cowalyow2012@gmail.com>
2022-04-19 17:12:16 +0300

    Chat in audio room

---
+++ frontend/src/components/chatComponent/fileLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <span v-if="files">
+    <a 
+        style="display: block;" 
+        v-for="file in files" 
+        :key="file.id" 
+        :href='`http://localhost:3000/file/download/${file.id}`'
+        >
+        ICON - {{file.fileName}}
+    </a>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['files']
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/Room/audioRoom.vue
+++ frontend/src/views/Room/audioRoom.vue
@@ -7,6 +7,7 @@
         <li v-for="user in roomUsers" :key="user.userId">User - {{user.userId}}. Mute status - {{!!user.mute}}</li>
     </ul>
     <free-users :roomId='roomId' :users="testUser" />
+    <chat-room-container/>
     <div ref="audioGroup"></div>
 </template>
 
@@ -17,10 +18,11 @@ import {  reactive, onUnmounted, ref, inject, watch } from 'vue'
 
 import $api from '../../axios'
 import freeUsers from '../../components/freeUsers.vue'
+import chatRoomContainer from '../../components/chatRoomContainer.vue'
 
 import Peer from 'peerjs';
 export default {
-    components: { freeUsers },
+    components: { freeUsers, chatRoomContainer },
     async setup() {
 
         // ==== vars ==== //
