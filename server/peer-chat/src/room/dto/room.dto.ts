c1f6ce5280674650808d2ddee92feb0868c45d1d
zendau <cowalyow2012@gmail.com>
2022-05-28 12:36:50 +0300

    fixed url parser

--- server/peer-chat/src/room/dto/room.dto.ts
+++ server/peer-chat/src/room/dto/room.dto.ts
@@ -13,5 +13,11 @@ export class roomDTO {
   })
   adminLogin: string;
 
+  @IsString()
+  @Length(2, 20, {
+    message: 'Value is smaller than 2 or bigger than 20 signs',
+  })
+  chatId: string;
+
   roomWithVideo: boolean;
 }
--- server/peer-chat/src/room/entities/room.entity.ts
+++ server/peer-chat/src/room/entities/room.entity.ts
@@ -9,6 +9,9 @@ export class Room {
   roomId: string;
 
   @Column()
+  chatId: string;
+
+  @Column()
   roomTitle: string;
 
   @Column()
