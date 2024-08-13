2916b6027480c16d13efb5ab599edb40157f2196
Zendau <cowalyow2012@gmail.com>
2024-08-14 00:50:33 +0300

    feat(server/peer-chat): omprove error handing

--- server/apps/peer-chat/src/room/room.controller.ts
+++ server/apps/peer-chat/src/room/room.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, HttpStatus, Logger } from '@nestjs/common';
+import { Controller, HttpStatus, Logger, UseFilters } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Room } from './entities/room.entity';
+import { DetailedRpcExceptionsFilter } from '@lib/exception';
 
+@UseFilters(new DetailedRpcExceptionsFilter())
 @Controller('room')
 export class RoomController {
   private readonly logger = new Logger(RoomController.name);
@@ -11,67 +13,32 @@ export class RoomController {
 
   @MessagePattern('room/add')
   async create(@Payload() createRoomData: Room) {
-    const res = await this.roomService.create(createRoomData).catch((err) => {
-      this.logger.error(err.sqlMessage);
-      return {
-        status: false,
-        message: 'error.unexpected',
-        httpCode: HttpStatus.BAD_REQUEST,
-      };
-    });
+    const res = await this.roomService.create(createRoomData);
     return res;
   }
 
   @MessagePattern('room/getByChat')
   async getByChatId(@Payload() chatId: string) {
     console.log(chatId);
-    const res = await this.roomService.getByChatId(chatId).catch((err) => {
-      this.logger.error(err.sqlMessage);
-      return {
-        status: false,
-        message: 'error.unexpected',
-        httpCode: HttpStatus.BAD_REQUEST,
-      };
-    });
+    const res = await this.roomService.getByChatId(chatId);
     return res;
   }
 
   @MessagePattern('room/list')
   async findOne(@Payload() idList: string[]) {
-    const res = await this.roomService.getByList(idList).catch((err) => {
-      this.logger.error(err.sqlMessage);
-      return {
-        status: false,
-        message: 'error.unexpected',
-        httpCode: HttpStatus.BAD_REQUEST,
-      };
-    });
+    const res = await this.roomService.getByList(idList);
     return res;
   }
 
   @MessagePattern('room/edit')
   async update(@Payload() updateRoomData: Room) {
-    const res = await this.roomService.update(updateRoomData).catch((err) => {
-      this.logger.error(err.sqlMessage);
-      return {
-        status: false,
-        message: 'error.unexpected',
-        httpCode: HttpStatus.BAD_REQUEST,
-      };
-    });
+    const res = await this.roomService.update(updateRoomData);
     return res;
   }
 
   @MessagePattern('room/delete')
   async remove(@Payload() roomId: string) {
-    const res = await this.roomService.remove(roomId).catch((err) => {
-      this.logger.error(err.sqlMessage);
-      return {
-        status: false,
-        message: 'error.unexpected',
-        httpCode: HttpStatus.BAD_REQUEST,
-      };
-    });
+    const res = await this.roomService.remove(roomId);
     return res;
   }
 }
